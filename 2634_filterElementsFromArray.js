ee7d4b7a1101283907c9c97b59b669930d76d48e
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-02-11 00:05:06 +0600

    2634 done

---
+++ 2634_filterElementsFromArray.js
@@ -0,0 +1,26 @@
+function filter(arr,fn){
+    const filteredArr = [];
+    for(let i =0; i<arr.length; i++){
+        if(fn(arr[i], i)){
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
+
+const arr1 = [0,10,20,30];
+const fn1 = function greaterThan10(n){
+    return n>10;
+}
+const arr2 = [1,2,3];
+const fn2 = function firstIndex(n,i){
+    return i === 0;
+}
+const arr3 = [-2,-1,0,1,2];
+const fn3 = function plusOne(n){
+    return n+1;
+}
+
+console.log(filter(arr1, fn1));
+console.log(filter(arr2, fn2));
+console.log(filter(arr3, fn3));
