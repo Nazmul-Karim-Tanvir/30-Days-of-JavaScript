6f16ac3e00ae2c824d047e8894d09830c054536a
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-02-10 23:19:05 +0600

    2635 complete

--- 2635_applyTransformOverArray.js
+++ 2635_applyTransformOverArray.js
@@ -1,4 +1,4 @@
-function applyTransformOverArray(arr, fn) {
+var map = function(arr, fn) {
     const result = [];
     for (let i = 0; i < arr.length; i++) {
         result.push(fn(arr[i], i));
@@ -8,7 +8,7 @@ function applyTransformOverArray(arr, fn) {
 
 // Test cases
 const arr1 = [1, 2, 3];
-const fn1 = function plusone(n) {
+const fn1 = function plusone(n,i) {
     return n + 1;
 };
 const fn2 = function plusI(n, i) {
@@ -17,6 +17,6 @@ const fn2 = function plusI(n, i) {
 const fn3 = function constant() {
     return 42;
 };
-console.log(applyTransformOverArray(arr1, fn1)); // Output: [2, 3, 4]
-console.log(applyTransformOverArray(arr1, fn2)); // Output: [1, 3, 5]
-console.log(applyTransformOverArray(arr1, fn3)); // Output: [42, 42, 42]
+console.log(map(arr1, fn1)); // Output: [2, 3, 4]
+console.log(map(arr1, fn2)); // Output: [1, 3, 5]
+console.log(map(arr1, fn3)); // Output: [42, 42, 42]
